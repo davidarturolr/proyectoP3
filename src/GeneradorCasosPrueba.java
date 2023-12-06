c6b86ed752834a7f9b33e0348d1997473df84a33
David Leon <da.leon@uniandes.edu.co>
2023-12-05 22:31:18 -0500

    final

--- src/GeneradorCasosPrueba.java
+++ src/GeneradorCasosPrueba.java
@@ -1,20 +1,29 @@
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.HashSet;
 import java.util.Random;
+import java.util.Set;
 
 public class GeneradorCasosPrueba {
 
-    public static void generarCasosPrueba(String archivoSalida, int numCasos) {
+    public static void generarCasosPrueba(String archivoSalida, int numCasos, int maxPersonas) {
         try (PrintWriter writer = new PrintWriter(new FileWriter(archivoSalida))) {
             writer.println(numCasos);
 
-            for (int i = 0; i < numCasos; i++) {
-                int numPersonas = 5000; // Cambia estos valores según tus necesidades
+            Set<Integer> numerosPersonas = new HashSet<>();
+            Random random = new Random();
+
+            for (int i = 0; i < numCasos - 1; i++) {
+                int numPersonas;
+                do {
+                    numPersonas = getRandomInt(1, maxPersonas);
+                } while (numerosPersonas.contains(numPersonas));
+                numerosPersonas.add(numPersonas);
 
                 // Generar montos aportados
                 for (int j = 0; j < numPersonas; j++) {
-                    writer.print(getRandomInt(100, 2000) + " ");
+                    writer.print(getRandomInt(100, 1000) + " ");
                 }
                 writer.println();
 
@@ -28,6 +37,25 @@ public class GeneradorCasosPrueba {
                     writer.println();
                 }
             }
+
+            // Asegurar al menos un caso con 10 mil personas
+            int numPersonas = maxPersonas;
+            // Generar montos aportados
+            for (int j = 0; j < numPersonas; j++) {
+                writer.print(getRandomInt(100, 1000) + " ");
+            }
+            writer.println();
+
+            // Generar conocidos para cada persona
+            for (int j = 1; j <= numPersonas; j++) {
+                int numConocidos = getRandomInt(1, numPersonas - 1);
+
+                for (int k = 0; k < numConocidos; k++) {
+                    writer.print(getRandomInt(1, numPersonas) + " ");
+                }
+                writer.println();
+            }
+
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -38,8 +66,8 @@ public class GeneradorCasosPrueba {
     }
 
     public static void main(String[] args) {
-        // Cambia el valor de 'numCasos' según la cantidad de casos de prueba que deseas generar
-        int numCasos = 1;
-        generarCasosPrueba("caso_prueba5k.txt", numCasos);
+        int numCasos = 50;
+        int maxPersonas = 10000;
+        generarCasosPrueba("casos_prueba10k.txt", numCasos, maxPersonas);
     }
 }
